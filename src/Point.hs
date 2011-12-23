913f093db90bfff3a00964c9351536b34fac3b86
Brendan Hickey <brendan@bhickey.net>
2011-12-23 01:08:30 -0500

    Points are all typeclassy

--- src/Point.hs
+++ src/Point.hs
@@ -5,13 +5,52 @@ import Direction
 newtype Point = Point { point :: (Int, Int) } deriving (Show, Eq, Ord)
 
 instance Num Point where
-  fromInteger = (\ x -> Point (fromIntegral x, 0))::(Integer-> Point)
+  fromInteger 0 = Point (0,0)
+  fromInteger x' = 
+    let x = fromIntegral x'
+        r = (floor ((sqrt.fromIntegral) x) + 1) `div` 2 
+        ring_start = (2 * r - 1)^2
+        top_bnd = ring_start + (2 * r + 1)
+        rgt_bnd = ring_start + (4 * r + 1)
+        bot_bnd = ring_start + (6 * r + 1)
+        offset = x - ring_start in
+      if x < top_bnd
+      then Point (-r + offset,r)
+      else if x < rgt_bnd
+           then Point (r, top_bnd - x + r - 1)
+           else if x < bot_bnd
+                then Point (r - (x - rgt_bnd) - 1, -r)
+                else Point (-r, x - bot_bnd - 1) 
   (Point (x1,y1)) + (Point (x2,y2)) = (Point ((x1 + x2), (y1 + y2)))
   (Point (x1,y1)) * (Point (x2,y2)) = (Point ((x1 * x2), (y1 * y2))) -- This makes no sense
   (Point (x1,y1)) - (Point (x2,y2)) = (Point ((x1 - x2), (y1 - y2)))
   abs (Point (x,y)) = (Point (abs x, abs y))
   signum (Point (x,y)) = (Point (signum x, signum y))
 
+instance Real Point where
+  toRational = toRational.toInteger
+
+instance Enum Point where
+  toEnum = fromInteger.fromIntegral
+  fromEnum = fromIntegral.toInteger
+
+instance Integral Point where
+  quotRem (Point (x1,y1)) (Point (x2,y2)) =  -- do you have a better idea?
+      (Point (quot x1 x2, quot y1 y2),
+       Point (rem x1 x2, rem y1 y2))
+  toInteger (Point (0,0)) = 0
+  toInteger (Point (x,y)) = 
+    let x' = fromIntegral x
+        y' = fromIntegral y
+        ring = max (abs x') (abs y')
+        off = (2 * ring - 1)^2 in
+      if ring == y'
+      then x' + ring + off
+      else if ring == (abs y')
+           then (4 * ring) + off - x'
+           else if x > 0
+                then (2 * ring) + x' - y' + off
+                else (5 * ring) + y' - (2 * x') + off
 
 zeroPoint :: Point
 zeroPoint = Point (0,0)
