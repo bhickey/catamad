1d77ee1cdc013efcbb7fcc83bcf33395af59588f
Brendan Hickey <brendan@bhickey.net>
2012-01-02 17:52:49 -0700

    Direction rotations

--- src/Direction.hs
+++ src/Direction.hs
@@ -1,5 +1,8 @@
  module Direction 
-   (Direction (..))
+   (Direction (..)
+   , turnLeft
+   , turnRight
+   , turnAround)
 where
 
 data Direction = 
@@ -12,6 +15,17 @@ data Direction =
   | West
   | NorthWest
   deriving (Bounded, Eq, Enum, Ord)
+
+turnLeft :: Direction -> Direction
+turnLeft North = NorthWest
+turnLeft d = pred d
+
+turnRight :: Direction -> Direction
+turnRight NorthWest = North
+turnRight d = succ d
+
+turnAround :: Direction -> Direction
+turnAround d = (turnLeft.turnLeft.turnLeft.turnLeft) d
 
 instance Show Direction where
   show North     = "N"
