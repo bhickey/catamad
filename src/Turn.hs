27c527cc5c8445db7ba3dcf289ab65f4ed375426
Brendan Hickey <brendan@bhickey.net>
2012-01-02 18:30:27 -0800

    Introduced the 'never' turn

--- src/Turn.hs
+++ src/Turn.hs
@@ -1,15 +1,20 @@
 module Turn (
   Visibility (..)
   ,Turn
+  ,never
   ,firstTurn
   ,nextTurn)
 where
 
-newtype Turn = Turn { turn :: Integer } deriving (Eq, Ord)
+data Turn = Turn Integer | Never deriving (Eq, Ord)
 newtype Visibility = Visibility { seenOn :: Turn }
 
 firstTurn :: Turn
-firstTurn = Turn { turn = 0 }
+firstTurn = Turn 0
+
+never :: Turn
+never = Never
 
 nextTurn :: Turn -> Turn
+nextTurn Never = Never
 nextTurn (Turn t) = Turn (t + 1)
