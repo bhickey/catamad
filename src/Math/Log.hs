f234ff62526ff48bead232b1c9c5a4c5f2beab06
Brendan Hickey <brendan@bhickey.net>
2012-01-03 22:25:24 -0800

    Discrete Log off by one

--- src/Math/Log.hs
+++ src/Math/Log.hs
@@ -3,4 +3,4 @@ module Math.Log where
 discreteLg :: Int -> Int -> [Int]
 discreteLg b p =
   snd $ foldl (\ (a,l) _ -> let r = (a * b) `mod` p in (r,r:l))
-              (1,[]) [1..p-1]
+              (1,[]) [0..p-1]
