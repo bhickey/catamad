0c7bec0c09bbfa1a3699f079fecbbc080cc93a1c
Brendan Hickey <brendan@bhickey.net>
2013-09-30 21:10:53 -0700

    Beliefs
    Add a storage mechanism for tracking entities that you have seen, but
    no longer see.

---
+++ src/Belief.hs
@@ -0,0 +1,32 @@
+module Belief where
+
+import Point
+import Data.Identifiable
+import qualified Data.Map as M
+import Data.Map (Map)
+
+data Beliefs a = Beliefs
+  (Map Point (Identifier a))
+  (Map (Identifier a) Point)
+
+get :: (Identifiable a) =>
+  Beliefs a ->
+  Point ->
+  Maybe (Identifier a)
+get (Beliefs pm _) = flip M.lookup pm
+
+find :: (Identifiable a) =>
+  Beliefs a ->
+  Identifier a ->
+  Maybe Point
+find (Beliefs _ im) = flip M.lookup im
+
+put :: (Identifiable a) =>
+  Beliefs a ->
+  (Identifier a, Point) ->
+  Beliefs a
+put (Beliefs pm im) (i, p) =
+  let pm' = case M.lookup i im of
+              Nothing -> M.insert p i pm
+              Just op -> M.insert p i (M.delete op pm) in
+    Beliefs pm' (M.insert i p im)
