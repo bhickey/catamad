7e28a1313972edc95c34aba0412a905bf9db11f5
Brendan Hickey <brendan@bhickey.net>
2013-09-30 19:15:49 -0700

    Don't store values in Identifiables
    Extract the unique Id and store it, rather than
    keeping the whole entity around.

--- src/Data/Identifiable.hs
+++ src/Data/Identifiable.hs
@@ -1,15 +1,15 @@
 module Data.Identifiable (Identifier, Identifiable(..), mkIdentifier) where
 
-data Identifier a = Identifier a
+data Identifier a = Identifier Integer
 
 mkIdentifier :: (Identifiable a) => a -> Identifier a
-mkIdentifier = Identifier
+mkIdentifier = Identifier . identify
 
 instance Identifiable a => Eq (Identifier a) where
-  (Identifier x) == (Identifier y) = (identify x) == (identify y)
+  (Identifier x) == (Identifier y) = x == y
 
 instance Identifiable a => Ord (Identifier a) where
-  compare (Identifier x) (Identifier y) = compare (identify x) (identify y)
+  compare (Identifier x) (Identifier y) = compare x y
 
 class Identifiable a where
   identify :: a -> Integer
